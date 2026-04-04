5b38df4431ecce056e1a8c49096f363edab40c27
lahieuphong2111 <hieuphong144@gmail.com>
2026-04-05 01:45:46 +0700

    fix(config): set basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'btmt-photobooth'
+
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 }
 
 export default nextConfig
